51ed1fd8f89626124987baba58ecddb31a97aecd
Magnus Jonsson <magnus@hpc2n.umu.se>
2018-09-12 22:43:13 +0200

    Adds record view

---
+++ src/views/Record.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="record">
+    <Paddle :wpm=15 />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Paddle from '@/components/Paddle.vue'
+
+export default {
+  name: 'record',
+  components: {
+    Paddle
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
